1d7f46147f336a5bea19c39d1150dedbcc471107
ArbalesterCZ <ArbalesterCZ@gmail.com>
2025-08-27 09:34:36 +0200

    formating update.

--- src/players.py
+++ src/players.py
@@ -7,13 +7,13 @@ class Players:
         self.__raw_data = numpy.genfromtxt(fname=filepath, dtype='str', delimiter='\n')
         self.__results = self.__raw_data.copy().tolist()
 
-        biggest = 0
+        name_length = 0
         for i in range(len(self.__raw_data)):
-            if len(self.__raw_data[i]) > biggest:
-                biggest = len(self.__raw_data[i])
+            if len(self.__raw_data[i]) > name_length:
+                name_length = len(self.__raw_data[i])
 
-        self.__space_after = ' ' * (24 - biggest)
-
+        self.__void_mass = name_length + 4
+        self.__void_size = ' ' * (self.__void_mass - name_length)
 
     def process(self, processes):
         print()
@@ -38,7 +38,7 @@ class Players:
 
         result = []
         for i in range(0, len(players) - 1, 2):
-            result.append('{one:<24}versus{space}{two}'.format(one=players[i], space=self.__space_after, two=players[i + 1]))
+            result.append(f'{players[i]:<{self.__void_mass}}versus{self.__void_size}{players[i + 1]}')
 
         if len(players) % 2 == 1:
             result.append('{player} is waiting..'.format(player=players[len(players) - 1]))
@@ -51,7 +51,7 @@ class Players:
         result = []
         for i in range(len(self.__raw_data)):
             for j in range(0, i):
-                result.append('{one:<24}versus{space}{two}'.format(one=self.__raw_data[i], space=self.__space_after, two=self.__raw_data[j]))
+                result.append(f'{self.__raw_data[i]:<{self.__void_mass}}versus{self.__void_size}{self.__raw_data[j]}')
 
         numpy.random.shuffle(result)
         self.__results = result
